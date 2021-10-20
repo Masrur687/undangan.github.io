54afba0398b8a19563105704dadf3d8009c63acb
Your Name <you@example.com>
2021-10-19 18:11:41 -0800

    undangan pertama

---
+++ index.php
@@ -0,0 +1,583 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	<meta charset="utf-8">
+	<title>Wedding of Fulan & Fulanah</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<!-- font awesome css -->
+	<link rel="stylesheet" type="text/css" href="css/font.awesome.css">
+	<!-- main css -->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<!-- responsive.css -->
+	<link rel="stylesheet" type="text/css" href="css/responsive.css">
+	<!-- default skin -->
+	<link rel="stylesheet" type="text/css" href="css/pink.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <!-- deal time -->
+  <link rel="stylesheet" type="text/css" href="css/style2.css">
+</head>
+<body>
+	<!-- header start -->
+	<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v11.0" nonce="eOBheS0V"></script>
+	<header class="header">
+		<div class="container">
+			<div class="row align-items-center justify-content-between">
+				<div class="logo">
+					<a href="index.html">F & F</a>
+				</div>
+				<div class="hamburger-btn">
+					<span></span>
+				</div>
+				<div class="nav">
+					<ul>
+						<li><a href="#" data-scroll-nav="0" class="active">Home</a></li>
+						<li><a data-scroll-nav="1" href="#">Couple</a></li>
+						<li><a data-scroll-nav="2" href="#">Story</a></li>
+						<li><a data-scroll-nav="3" href="#">Events</a></li>
+						<li><a data-scroll-nav="4" href="#">Lokasi</a></li>
+						<li><a data-scroll-nav="5" href="#">Gallery</a></li>
+						<li><a data-scroll-nav="6" href="#">RSVP</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!-- header end -->
+
+	<!-- home section start -->
+	<section class="home-section" data-scroll-index="0">>
+		<!-- slide start -->
+		<div class="slide active" style="background-image: url('img/slides/slide-1.jpg');">
+			<div class="container">
+		<div class="row align-items-center">
+			<div class="home-content">
+			<p>Undangan Walimatul Ursy</p>
+			<h1>Fulan & Fulanah</h1>
+			<span>03 Dec 2021</span>
+			</div>
+		</div>
+	</div>
+</div>
+		<!-- slide end -->
+		<!-- slide start -->
+		<div class="slide " style="background-image: url('img/slides/slide-2.jpg');">
+			<div class="container">
+		<div class="row align-items-center">
+			<div class="home-content">
+			<p>Undangan Walimatul Ursy</p>
+			<h1>Fulan & Fulanah</h1>
+			<span>03 Dec 2021</span>
+			</div>
+		</div>
+	</div>
+</div>
+		<!-- slide end -->
+		<!-- slide start -->
+		<div class="slide" style="background-image: url('img/slides/slide-3.jpg');">
+			<div class="container">
+		<div class="row align-items-center">
+			<div class="home-content">
+			<p>Undangan Walimatul Ursy</p>
+			<h1>Fulan & Fulanah</h1>
+			<span>03 Dec 2021</span>
+			</div>
+		</div>
+	</div>
+</div>
+		<!-- slide end -->
+	</section>
+	<!-- home section end -->
+
+<!-- ayat section start -->
+<section class="ayatsuci-section">
+	<div class="container">
+		<div class="row">
+		<div class="ayat-title">
+			<h1>kepada yang terhormat,<br> <?php echo $_GET['name'];?></h1><br>
+			<p>Dengan penuh rasa hormat, kami mengundang bapak/ibu/saudara(i) untuk hadir memberi doa restu pada pernikahan kami. <br>Atas kehadiran serta doa restu anda, kami ucapkan terima kasih.</p>	
+		</div>
+		</div>
+		
+</section>
+<!-- ayat section end -->
+
+<!-- couple section start -->
+<section class="couple-section" data-scroll-index="1">
+	<div class="container">
+		<div class="row">
+			<div class="section-title">
+				<h2>Pengantin</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="couple"><img src="img/pria.jpg" alt="happy couple">
+			<h3>Fulan</h3>
+			<p>Putra Bpk. Dewa dan Ibu Dewi</p>
+			<p>Ds. Pakisrejo Kec. Rejotangan Kab. Tulungagung</p>
+				<div class="social-links">
+					<a href="#" title="facebook">
+						<i class="fab fa-facebook-f"></i>
+					</a>
+					<a href="#" title="twitter">
+						<i class="fab fa-twitter"></i>
+					</a>
+					<a href="#" title="instagram">
+						<i class="fab fa-instagram"></i>
+					</a>
+					<a href="#" title="linkedin">
+						<i class="fab fa-linkedin"></i>
+					</a>
+				</div>
+			</div>
+			<div class="couple">
+			<i class="fas fa-heart"></i>
+			<img src="img/wanita.jpg" alt="happy couple">
+			<h3>Fulannah</h3>
+			<p>Putri Bpk. Dewa dan Ibu Dewi</p>
+			<p>Ds. Pakisrejo Kec. Rejotangan Kab. Tulungagung</p>
+				<div class="social-links">
+					<a href="#" title="facebook">
+						<i class="fab fa-facebook-f"></i>
+					</a>
+					<a href="#" title="twitter">
+						<i class="fab fa-twitter"></i>
+					</a>
+					<a href="#" title="instagram">
+						<i class="fab fa-instagram"></i>
+					</a>
+					<a href="#" title="linkedin">
+						<i class="fab fa-linkedin"></i>
+					</a>
+				</div>
+			</div>
+			
+			
+		</div>
+	</div>
+</section>
+<!-- couple section end -->
+<!-- story section start -->
+<section class="story-section" data-scroll-index="2">
+	<div class="container">
+		<div class="row">
+			<div class="section-title">
+				<h2>Our Love Story</h2>
+			</div>
+		</div>
+	<div class="row">
+		<div class="story-content">
+			<div class="row">
+			<!-- story item start-->	
+			<div class="story-item">
+				<i class="fas fa-heart"></i>
+				<div class="story-text">
+					<h3>First Meet</h3>
+					<span class="date">29 Feb 2020</span>
+					<p>Masjid yang agung di kab. Tulungagung menjadi sebuah momen indah yang mempertemukan cinta kami<p>
+				</div>
+				<div class="story-img">
+					<img src="img/story/story-1.jpg" alt="couple
+					lover story">
+				</div>
+			</div>	
+			<!-- story item end-->
+			<!-- story item 2 start-->	
+			<div class="story-item">
+				<i class="fas fa-heart"></i>
+				<div class="story-text">
+					<h3>First Date</h3>
+					<span class="date">26 Jun 2020</span>
+					<p>Hari itu ku temui engkau dirumahmu, dan aku nyatakan kepada orang tuamu tentang niat baik yang aku pendam semenjak pertama kali bertemu denganmu <p>
+				</div>
+				<div class="story-img">
+					<img src="img/story/story-2.jpg" alt="couple
+					lover story">
+				</div>
+			</div>	
+			<!-- story item 2 end-->
+			<!-- story item 3 start-->	
+			<div class="story-item">
+				<i class="fas fa-heart"></i>
+				<div class="story-text">
+					<h3>Proposal</h3>
+					<span class="date">27 Jun 2020</span>
+					<p>Bahagiaku saat mendengar enkau dan orang tuamu mau menerima dan memberikan restu kepadaku untuk menikahimu<p>
+				</div>
+				<div class="story-img">
+					<img src="img/story/story-3.jpg" alt="couple
+					lover story">
+				</div>
+			</div>	
+			<!-- story item 3 end-->
+			<!-- story item 4 start-->	
+			<div class="story-item">
+				<i class="fas fa-heart"></i>
+				<div class="story-text">
+					<h3>Engangement</h3>
+					<span class="date">29 Jun 2020</span>
+					<p>Sebuah hari yang sangat aku dan engkau nantikan telah tiba. Semoga Allah SWT, selalu meridhoi setiap jalan kita dalam beribadah kepadaNya <p>
+				</div>
+				<div class="story-img">
+					<img src="img/story/story-4.jpg" alt="couple
+					lover story">
+				</div>
+			</div>	
+			<!-- story item 4 end-->
+ 			</div>
+ 		</div>
+ 	</div>
+ </div>
+</section>
+<!-- story section end -->
+
+<!-- event section start -->
+<section class="event-section" data-scroll-index="3">
+	<div class="container">
+		<div class="row">
+			<div class="section-title">
+				<h2>Events</h2>
+			</div>
+		</div>
+		<div class="row justify-content-center">
+			<!--item start--->
+			<div class="event-item">
+				<div class="event-item-inner">
+					<h3>Akad Nikah</h3>
+					<span class="date">03 Dec 2021</span>
+					<span class="time">08.00 - 09.00 WIB</span>
+					<p>{Tempat}</p>
+					<p>Di Rt.01 Rw.02 Dsn. Nakeran Ds. Pakisrejo Kec. Rejotangan Kab. Tulungagung</p>
+					<!--<img src="img/icons/flower.svg" alt="event">--->
+			</div>
+		</div>
+		<!--item end--->
+		<!--item start--->
+			<div class="event-item">
+				<div class="event-item-inner">
+					<h3>Resepsi</h3>
+					<span class="date">03 Dec 2021</span>
+					<span class="time">10.00 - 11.00 WIB</span>
+					<p>{Tempat}</p>
+					<p>Di Rt.01 Rw.02 Dsn. Nakeran Ds. Pakisrejo Kec. Rejotangan Kab. Tulungagung</p>
+					<!--<img src="img/icons/flower.svg" alt="event">--->
+			</div>
+		</div>
+		<!--item end--->
+	</div>
+
+	<!--deal time start-->
+	<div class="deal" id="deal">
+
+    
+
+    <div class="row">
+
+        <div class="content">
+           
+            <h3 class="text">deal of the day</h3>
+            <div class="count-down">
+                <div class="box">
+                    <h3 id="days">18</h3>
+                    <span>Hari</span>
+                </div>
+                <div class="box">
+                    <h3 id="hours">23</h3>
+                    <span>Jam</span>
+                </div>
+                <div class="box">
+                    <h3 id="minutes">00</h3>
+                    <span>Menit</span>
+                </div>
+                <div class="box">
+                    <h3 id="seconds">00</h3>
+                    <span>Detik</span>
+                </div>
+            </div>
+            
+        </div>
+	</div>
+	<!--deal time-->
+</div>
+</div>
+</section>
+
+
+
+<!--maps start-->
+	<!-- map section start -->
+<section class="map-section"data-scroll-index="4">
+	<div class="container">
+		<div class="row">
+		<div class="section-title">
+			<h2>Lokasi Resepsi</h2>
+			<p>Dsn. Nakeran Ds. Pakisrejo Kec. Rejotangan Kab. Tulungagung</p>
+		</div>			
+		</div>
+		<div class="row">
+			<div class="map-form">
+				<form class="form">
+						
+					<div class="row">
+						<div class="input-group">
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3949.3294977763026!2d112.0761598!3d-8.169521099999987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78e8439e4df3d7%3A0xa954b5ff8e2ebc4e!2sWana%20Wisata%20Alam%20Kandung!5e0!3m2!1sid!2sus!4v1630531598489!5m2!1sid!2sus" width="100%" height="450" style="border:0;" allowfullscreen=""></iframe>
+
+						</div>
+					</div>
+					<div class="input-group">
+							<a href="https://goo.gl/maps/YboB5NcEstMZPUZS7" class="w3-btn w3-black">Menuju lokasi</a>
+						</div>			
+				</form>				
+			</div>			
+		</div>		
+	</div>
+</section>
+
+<!-- map section end -->
+<!--maps end-->
+
+<!-- event section end -->
+<section class="gallery-section" data-scroll-index="5">
+	<div class="container">
+		<div class="row">
+			<div class="section-title">
+				<h2>Gallery Foto</h2>
+			</div>
+		</div>
+		<div class="row">
+			<!-- gallery item start -->
+			<div class="gallery-item">
+				<div class="gallery-item-inner">
+					<img src="img/gallery/thumb/1.jpg" data-large="img/gallery/large/1.jpg" alt="couple gallery">
+				</div>
+			</div>
+			<!-- gallery item end -->
+			<!-- gallery item start -->
+			<div class="gallery-item">
+				<div class="gallery-item-inner">
+					<img src="img/gallery/thumb/2.jpg" data-large="img/gallery/large/2.jpg" alt="couple gallery">
+				</div>
+			</div>
+			<!-- gallery item end -->
+			<!-- gallery item start -->
+			<div class="gallery-item">
+				<div class="gallery-item-inner">
+					<img src="img/gallery/thumb/3.jpg" data-large="img/gallery/large/3.jpg" alt="couple gallery">
+				</div>
+			</div>
+			<!-- gallery item end -->
+			<!-- gallery item start -->
+			<div class="gallery-item">
+				<div class="gallery-item-inner">
+					<img src="img/gallery/thumb/4.jpg" data-large="img/gallery/large/4.jpg" alt="couple gallery">
+				</div>
+			</div>
+			<!-- gallery item end -->
+			<!-- gallery item start -->
+			<div class="gallery-item">
+				<div class="gallery-item-inner">
+					<img src="img/gallery/thumb/5.jpg" data-large="img/gallery/large/5.jpg" alt="couple gallery">
+				</div>
+			</div>
+			<!-- gallery item end -->
+			<!-- gallery item start -->
+			<div class="gallery-item">
+				<div class="gallery-item-inner">
+					<img src="img/gallery/thumb/6.jpg" data-large="img/gallery/large/6.jpg" alt="couple gallery">
+				</div>
+			</div>
+			<!-- gallery item end -->
+		</div>
+	</div>	
+</section>
+
+<!-- galery section end -->
+
+
+<!-- RSVP section start -->
+<div class="container-contact100" data-scroll-index="6">
+    <div class="wrap-contact100">
+      <form class="contact100-form validate-form" id="whatsapp">
+        <span class="contact100-form-title">Konfirmasi kehadiran</span>
+        <input class="tujuan" type="hidden" id="noAdmin">
+        <div class="wrap-input100">
+          <span class="label-input100">Nama</span>
+          <label>
+          <input class="input100 nama" type="text" placeholder="Nama anda">
+          </label>
+          <span class="focus-input100"></span>
+        </div>
+
+        <div class="wrap-input100">
+          <span class="label-input100">Alamat</span>
+          <label>
+          <input class="input100 alamat" placeholder=" Alamat lengkap anda">
+          </label>
+          <span class="focus-input100"></span>
+        </div>
+        
+        <div class="wrap-input100">
+          <span class="label-input100">Konfirmasi</span>
+          <label>
+          <input class="input100 konfirmasi" type="text" placeholder="Hadir/Tidak hadir">
+          </label>
+          <span class="focus-input100"></span>
+        </div>
+
+        <div class="wrap-input100">
+          <span class="label-input100">Jumlah Hadir</span>
+          <label>
+          <input class="input100 jumlah" type="text" placeholder="-">
+          </label>
+          <span class="focus-input100"></span>
+        </div>
+
+
+        
+        <div class="container-contact100-form-btn">
+          <div class="wrap-contact100-form-btn">
+            <div class="contact100-form-bgbtn"></div>
+            <a class="contact100-form-btn submit">Kirim</a>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+  <!-- RSVP section end -->
+
+
+<div class="fb-comments" data-href="https://apxgqeukuealh7g5sxdbcg-on.drv.tw/www.musa125.com/#" data-width="100%" data-numposts="5"></div>
+
+<!--footer start-->
+<section class="footer-section">
+	<div class="container">
+		<div class="row">
+		<div class="section-title">
+			<p>Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia menciptakan pasangan-pasangan untukmu dari jenismu sendiri, agar kamu cenderung dan merasa tenteram kepadanya, dan Dia menjadikan di antaramu rasa kasih dan sayang. Sungguh, pada yang demikian itu benar-benar terdapat tanda-tanda (kebesaran Allah) bagi kaum yang berpikir.<br/>(Ar-Rum 21)</p>
+			<br>
+			<p1>Hormat kami,</p1>
+			<h2>Fulan & Fulannah</h2>		
+		</div>
+		</div>
+		<div class="row">
+		<div class="copyright-title">
+			<p>ALMASROUR GRAPHIC DESAIGNER</p>	
+			<a href="https://www.facebook.com/profile.php?id=100071830567101" target= "_blank">Facebook: Digital Wedding Invitation Tulungagung</a>
+			<a href="https://wa.me/+6285603049583" class="w3-btn w3-black">WhatsApp: 085-603-049-583</a>
+		</div>
+		</div>
+</section>
+<!--footer end-->
+
+<!--copy right start-->
+<!--<section class="copyright-section">
+	<div class="container">
+		<div class="row">
+		<div class="section-title">
+			<p>Almasrour Graphic Desaigner<br>
+			FB: Digital Wedding Invitation Tulungagung<br> WA: 085-603-049-583</p>		
+		</div>
+		</div>
+	</div>
+</section>-->
+<!--copyright end-->
+
+
+<!-- galery popup start -->
+<div class="gallery-popup">
+	<div class="gp-container">
+		<div class="gp-counter"></div>
+		<div class="gp-close">&times;</div>
+		<img src="img/gallery/large/1.jpg" class="gp-img" alt="gallery-img">
+		<div class="gp-controls">
+		<div class="prev">
+			<i class="fas fa-angle-left"></i>
+		</div>
+		<div class="next">
+			<i class="fas fa-angle-right"></i>
+		</div>
+	</div>
+</div>
+</div>
+<!-- galery popup end -->
+
+<!--audio start-->
+<audio id="myAudio" loop="loop" src="flymetothemoon.mp3" type="audio/mp3"></audio>
+<i class="fa fa-music pause"></i>
+<!--audio end-->
+
+<!--jquery-->
+  <script>
+    $("#noAdmin").val("085603049583");
+$('.whatsapp-btn').click(function() {
+  $('#whatsapp').toggleClass('toggle');
+});
+// Onclick Whatsapp Sent!
+$('#whatsapp .submit').click(WhatsApp);
+$("#whatsapp input, #whatsapp textarea").keypress(function() {
+  if (event.which == 13) WhatsApp();
+});
+var reg = /^([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})+\.([A-Za-z]{2,4})+\@([A-Za-z0-9_\-\.])$/;
+
+function WhatsApp() {
+  var ph = '';
+  if ($('#whatsapp .nama').val() == '') { // Cek Nama
+    ph = $('#whatsapp .nama').attr('placeholder');
+    alert('Silahkan tulis ' + ph);
+    $('#whatsapp .nama').focus();
+    return false;
+  } 
+  	else if ($('#whatsapp .alamat').val() == '') { // Cek Alamat
+    ph = $('#whatsapp .alamat').attr('placeholder');
+    alert('Silahkan tulis ' + ph);
+    $('#whatsapp .alamat').focus();
+    return false;
+  } 
+  	else if ($('#whatsapp .konfirmasi').val() == '') { // Cek Alamat
+    ph = $('#whatsapp .konfirmasi').attr('placeholder');
+    alert('Silahkan tulis ' + ph);
+    $('#whatsapp .konfirmasi').focus();
+    return false;
+  } 
+  	else if ($('#whatsapp .jumlah').val() == '') { // Cek Whatsapp
+    ph = $('#whatsapp .jumlah').attr('placeholder');
+    alert('Silahkan tulis ' + ph);
+    $('#whatsapp .jumlah').focus();
+    return false;
+  }
+  else{
+    // Check Device (Mobile/Desktop)
+    var url_wa = 'https://web.whatsapp.com/send';
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      url_wa = 'whatsapp://send/';
+    }
+    // Get Value
+    var tujuan = $('#whatsapp .tujuan').val(),
+      nama = $('#whatsapp .nama').val(),
+      alamat = $('#whatsapp .alamat').val(),
+      konfirmasi = $('#whatsapp .konfirmasi').val(),
+      jumlah = $('#whatsapp .jumlah').val();
+
+    $(this).attr('href', url_wa + '?phone=62 ' + tujuan + '&text=Nama: ' + nama + '%0AAlamat: ' + alamat + '%0AKonfirmasi: ' + konfirmasi +' %0AJumlah: ' + jumlah + ' %0A%0Adesaignedby_almasrour_graphic_desaigner '  );
+    var w = 960,
+      h = 540,
+      left = Number((screen.width / 2) - (w / 2)),
+      tops = Number((screen.height / 2) - (h / 2)),
+      popupWindow = window.open(this.href, '', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=1, copyhistory=no, width=' + w + ', height=' + h + ', top=' + tops + ', left=' + left);
+    popupWindow.focus();
+    return false;
+  }
+}
+  </script>
+<script src="js/jquery.min.js"></script>	
+<!--main-->
+<script src="js/main.js"></script>
+<!--scrooll-->
+<script src="js/scrollIt.min.js"></script>
+<script src="text/javascript.js">
+	document.addEventListener("contextmenu",function(undangan){
+undangan.preventDefault();
+});
+	</script>
+</body>
+</html>
